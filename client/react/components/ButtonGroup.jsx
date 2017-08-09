64aef434418cabd70bb28c2a29cd62c023ee57ff
Marcelo Blechner <marcblec@br.ibm.com>
2017-08-09 11:08:35 -0300

    Getting rid of a React warning.

--- client/react/components/ButtonGroup.jsx
+++ client/react/components/ButtonGroup.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 const ButtonGroup = ({ buttons, centered }) => (
   <div className={`bx--form-item ${centered ? 'centered' : ''}`}>
-    {buttons.map(({ label, onClick, disabled }) => (
+    {buttons.map(({ label, onClick, disabled }, i) => (
       <button
+        key={i}
         className="bx--btn bx--btn--primary"
         type="button"
         onClick={onClick}
