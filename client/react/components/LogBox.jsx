19a91f4779db9c5e946ca854dff6730938bb31dc
Marcelo Blechner <marcblec@br.ibm.com>
2017-08-10 14:42:27 -0300

    Fixing the log auto scroll issue.

--- client/react/components/LogBox.jsx
+++ client/react/components/LogBox.jsx
@@ -1,9 +1,9 @@
 // external imports
-import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 
 class LogBox extends Component {
   componentDidMount() {
+    this.previousScrollHeight = this.logBox.scrollHeight;
     this.scrollToBottom();
   }
 
@@ -12,16 +12,17 @@ class LogBox extends Component {
   }
 
   scrollToBottom = () => {
-    const node = ReactDOM.findDOMNode(this.logEnd);
-    node.scrollIntoView({ behavior: 'smooth' });
+    const scrolledBottom = ((this.logBox.offsetHeight + this.logBox.scrollTop) >= this.previousScrollHeight);
+    // If its on bottom continue the scroll on bottom
+    if (scrolledBottom) this.logBox.scrollTop = this.logBox.scrollHeight;
+    this.previousScrollHeight = this.logBox.scrollHeight;
   }
 
   render = () => (
     <div className="bx--form-item">
       <label htmlFor="log-box" className="bx--label">{'Log'}</label>
-      <div id="log-box" className="log-box">
+      <div id="log-box" className="log-box" ref={el => { this.logBox = el; }}>
         {this.props.logs.map((log, i) => <p key={i}>{JSON.stringify(log)}</p>)}
-        <div style={{ float: 'left', clear: 'both' }} ref={el => { this.logEnd = el; }} />
       </div>
     </div>
   );
