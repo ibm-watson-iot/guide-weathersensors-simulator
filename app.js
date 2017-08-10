7602eb91be23f7f8ce55b1bc434adb5663958c52
Marcelo Blechner <marcblec@br.ibm.com>
2017-08-10 10:48:32 -0300

    Including deployment tracker.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict';
 
+// Comment out the line below if you don't want to track deployment of this application
+require('cf-deployment-tracker-client').track();
+
 const app = require('express')();
 require('./server')(__dirname)(app);
