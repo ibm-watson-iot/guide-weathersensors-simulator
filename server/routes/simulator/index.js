b8f98c57d9a7df69385c2c87df928abc7c6f3549
Marcelo Blechner <marcblec@br.ibm.com>
2017-08-17 16:23:48 -0300

    Catching error when running the simulator. There is no need to handle this error because all logs are sent to the UI via socket.

--- server/routes/simulator/index.js
+++ server/routes/simulator/index.js
@@ -16,14 +16,14 @@ module.exports = (io, middlewares) => {
   router.post('/run', (req, res) => {
     // Return from the POST command regardless of the primise result
     // because the logs will be sent to the UI via socket
-    simulator.run(req);
+    simulator.run(req).catch(e => e);
     res.send({});
   });
 
   router.post('/stop', (req, res) => {
     simulator.stop()
       .then((result) => res.send({ result }))
-      .catch((e) => res.status(500).send({ error: true, message: e.message }));
+      .catch(e => res.status(500).send({ error: true, message: e.message }));
   });
 
   return router;
